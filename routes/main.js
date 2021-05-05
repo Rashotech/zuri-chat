6887684c17642028a9edc5b5f3e898a360e1e13c
Rashotech <rashotech@gmail.com>
2021-05-05 12:06:34 +0100

    chat and chatroom route and contoller

--- routes/main.js
+++ routes/main.js
@@ -1,18 +1,44 @@
 const express = require('express');
 const router = express.Router();
 const { requireLogin } = require('../middleware/auth');
+const Room = require('../models/RoomModel');
+const Chat = require('../models/chatModel');
+const ObjectId = require('mongoose').Types.ObjectId;
 
-router.get('/', (req, res) => {
-    res.render('home');
+router.get('/', requireLogin, async (req, res) => {
+    var rooms = await Room.find({});
+    var room = await Room.findOne({ roomName: "General" });
+    var chats = await Chat.find({ roomId: room._id });
+    payload = {
+        "user": req.session.user.username,
+        "userLoggedIn": req.session.user,
+        "rooms": rooms,
+        "roomName": room.roomName,
+        "currentRoom": room._id,
+        "chats": chats
+    }
+    res.render('home', payload);
 });
 
-router.get('/chat', requireLogin, (req, res) => {
+router.get('/chat/:id', requireLogin, async (req, res) => {
+    const roomId = req.params.id;
+    if(!ObjectId.isValid(roomId)) {
+        return res.render('error');
+    }
+    var room = await Room.findOne({ _id: roomId });
+    var rooms = await Room.find({});
+    var chats = await Chat.find({ roomId: roomId });
     payload = {
-        "pageTitle": "Chat",
-        "userLoggedIn": req.session.user
+        "user": req.session.user.username,
+        "userLoggedIn": req.session.user,
+        "rooms": rooms,
+        "roomName": room.roomName,
+        "currentRoom": room._id,
+        "chats": chats
     }
     res.render('chat', payload);
 });
+
 
 router.get('/logout', (req, res, next) => {
     if(req.session) {
@@ -23,3 +49,4 @@ router.get('/logout', (req, res, next) => {
 });
 
 module.exports = router;
+
