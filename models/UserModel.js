4bad4f5a108a1eaa920084f97094d069d02670ad
Rashotech <rashotech@gmail.com>
2021-05-05 12:05:17 +0100

    user model

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    firstName: { type: String, trim: true},
-    lastName: { type: String, trim: true},
+    fullName: { type: String, trim: true},
     username: { type: String, required: true, trim: true, unique: true},
-    email: { type: String, required: true, trim: true, lowercase: true, unique: true },
     password: { type: String }
 });
 
